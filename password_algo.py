c15027096759532e60fb4679c0ee1111b125953c
Samyukthan-A <asamyukthan1@gmail.com>
2024-11-09 07:36:13 -0800

    Create password_algo.py

---
+++ password_algo.py
@@ -0,0 +1,15 @@
+import binascii 
+import os
+#import any encryption liberies 
+os.system('cls')
+
+def hex_to_tex(hex):
+    return binascii.unhexlify(hex)
+
+key = "" #input your key here
+#f = (key)
+
+def de(ps):
+    text = hex_to_tex(ps)
+    a = f.decrypt(text)
+    return a
